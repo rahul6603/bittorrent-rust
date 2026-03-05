545c365168ee27f13474a9d26c9692137c37f71c
Rahul Pamnani <rahulpamnani6603@gmail.com>
2026-03-05 17:05:32 +0530

    Add decode argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-use serde_bencode;
+use serde_bencode::{self, value::Value as BencodeValue};
+use serde_json::{Value as JsonValue, json};
 use sha1::{Digest, Sha1};
 use std::{env, error::Error, fs};
 
@@ -28,6 +29,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let command = &args[1];
 
     match command.as_str() {
+        "decode" => {
+            let bencoded_contents = &args[2];
+            let decoded_contents: BencodeValue = serde_bencode::from_str(bencoded_contents)?;
+            let decoded_json = serde_json::to_string_pretty(&bencode_to_json(decoded_contents))?;
+            println!("{decoded_json}");
+        }
         "info" => {
             let torrent_file = &args[2];
             get_torrent_file_info(torrent_file)?;
@@ -35,6 +42,27 @@ fn main() -> Result<(), Box<dyn Error>> {
         _ => println!("unknown command: {}", args[1]),
     }
     Ok(())
+}
+
+fn bencode_to_json(bencoded_contents: BencodeValue) -> JsonValue {
+    match bencoded_contents {
+        BencodeValue::Bytes(bytes) => match String::from_utf8(bytes) {
+            Ok(str) => json!(str),
+            Err(e) => json!(hex::encode(e.into_bytes())),
+        },
+        BencodeValue::Int(int) => json!(int),
+        BencodeValue::List(list) => {
+            JsonValue::Array(list.into_iter().map(bencode_to_json).collect())
+        }
+        BencodeValue::Dict(d) => {
+            let mut map = serde_json::Map::new();
+            for (k, v) in d {
+                let key = String::from_utf8_lossy(&k).into_owned();
+                map.insert(key, bencode_to_json(v));
+            }
+            JsonValue::Object(map)
+        }
+    }
 }
 
 fn get_torrent_file_info(torrent_file: &String) -> Result<(), Box<dyn Error>> {
